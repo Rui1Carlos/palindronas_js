d41d7450adc79e7d8368ea23576483c851e42fc8
Rui Carlos Teixeira <7ruicarlos@gmail.com>
2026-02-19 12:26:24 +0100

    melhorado e criado e css para ele

--- testes.js
+++ testes.js
@@ -1,40 +1,49 @@
 
+let palavras = []
 
-
-function adicionarNome() {
-    nome = document.getElementById("nome").value
-    document.getElementById("name").innerHTML=nome
+function adicionarNome(n) {
+    document.getElementById("mensagem1").innerText = ""
+    let nome = document.getElementById("nome").value
+    palavras.push(nome)
+    document.getElementById("name").innerHTML = palavras.join(" - ")
+    document.getElementById("nome").value=""
 }
 
 
 
-let n
+
 function palindrona() {
-    let nomes = []
+    
     let palindronas = []
 
     
-        for (let i = 0; i < 3; i++) {
+        for (let i = 0; i < palavras.length; i++) {
         let nova = ""
-        let nome = prompt("Digite uma palavra: ");
-        nomes.push(nome);
-
-        for (let k = nome.length - 1; k >= 0; k--) {
-            nova += nome[k]
+            let palavra= palavras[i]
+        for (let k = palavra.length - 1; k >= 0; k--) {
+            nova += palavra[k]
         }
-        if (nova == nome) {
+        if (nova == palavra) {
             palindronas.push(nova)
         }
 
     }
     if (palindronas.length > 0) {
-        alert("estas são as palavras palindronas " + palindronas)
-        alert("foram encontradas " + palindronas.length + " palavras palindronas")
+        //alert("estas são as palavras palindronas " + palindronas)
+        //alert("foram encontradas " + palindronas.length + " palavras palindronas")
+        document.getElementById("mensagem1").innerText = "estas são as palavras palindronas " + palindronas
+        document.getElementById("mensagem2").innerText = "foram encontradas " + palindronas.length + " palavras palindronas"
     }
     else {
-        document.getElementById("mensagem").innerText="não tem palavras palindronas"
+        document.getElementById("mensagem1").innerText="não tem palavras palindronas"
     }
-     
     
+    
+}
+function limparTela() {
+    palavras = []
+    document.getElementById("name").innerHTML = ""
+    document.getElementById("mensagem1").innerText = ""
+    document.getElementById("mensagem2").innerText = ""
 }
 
